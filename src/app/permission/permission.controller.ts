b281ee5aba064080d8778d2862217ae51066b266
Cosimo Celeste <gogliacosimoceleste@gmail.com>
2022-10-20 11:57:25 +0200

    Update permission.controller.ts

--- src/app/permission/permission.controller.ts
+++ src/app/permission/permission.controller.ts
@@ -2,24 +2,25 @@ import { Controller, Res, Header, Get, Param } from '@nestjs/common';
 import { Response } from 'express';
 import { DatabaseProvider } from '../database/database.provider';
 import { ResponseUtils } from '../../utils/response.utils';
-import { ConfigProvider } from '../config/config.provider';
 import { PermissionEntity } from './entities/permission.entity';
 import { GetPermission } from './types/getpermisison.type';
 
 @Controller('/permission')
 export class PermisisonController {
-    private readonly _configProvider: ConfigProvider;
     private readonly _databaseProvider: DatabaseProvider;
 
-    constructor(configProvider: ConfigProvider, databseProvider: DatabaseProvider) {
-        this._configProvider = configProvider;
+    constructor(databseProvider: DatabaseProvider) {
         this._databaseProvider = databseProvider;
     }
 
     @Get('/get/:permission')
-    @Header('accept', 'application/json')
     @Header('content-type', 'application/json')
     public async getPermission(@Param('permission') permission: string, @Res() res: Response): Promise<void> {
+        if (permission.length == 0) {
+            res.statusCode = 400;
+            return;
+        }
+
         const permissionData: PermissionEntity = await this._databaseProvider.connection.getRepository(PermissionEntity).findOne({
             where: {
                 permissionName: permission
